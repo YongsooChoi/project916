f518f61825c88c1d513317f0633bdd996e6da960
YongsooChoi <44487784+YongsooChoi@users.noreply.github.com>
2018-11-22 22:15:34 +0900

    Update app.js
    배포를 위한 session cookie 설정 변경

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ const sessionOption =  {
 	secret: process.env.COOKIE_SECRET,
 	cookie: {
 		httpOnly: true,
-		secure: false,
+		secure: true,
 	},
 	store: new RedisStore({
 		host: process.env.REDIS_HOST,
