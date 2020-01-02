c37f8d633913ef49c17a7112dae1ecb424c3f537
Yongsoo Choi <44487784+YongsooChoi@users.noreply.github.com>
2020-01-02 22:46:43 +0900

    코드 정리

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
-var mysql      = require('mysql');
-var dbconfig   = require('../config/config.js');
+var mysql = require('mysql');
+var dbconfig = require('../config/config.js');
 var connection = mysql.createConnection(dbconfig);
 var app = express();
 
@@ -11,15 +11,12 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-app.get('/farmers', function(req, res){
-
+app.get('/farmers', function(req, res) {
 	  connection.query('SELECT * FROM farmers', function(err, rows) {
-	    if(err) throw err;
-
-	    console.log('The solution is: ', rows);
-	    res.send(rows);
+		  if(err) throw err;
+		  console.log('The solution is: ', rows);
+		  res.send(rows);
 	  });
-	});
-
+});
 
 module.exports = router;
